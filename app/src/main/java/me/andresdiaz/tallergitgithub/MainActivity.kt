aee672f830ee9699852463682af03fc90b5f550c
Andrés Díaz <andresda.diaz10@gmail.com>
2026-09-16 09:41:16 -0500

    feat: agrega imagen personal

--- app/src/main/java/me/andresdiaz/tallergitgithub/MainActivity.kt
+++ app/src/main/java/me/andresdiaz/tallergitgithub/MainActivity.kt
@@ -4,44 +4,98 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import me.andresdiaz.tallergitgithub.ui.theme.TallerGitGithubTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TallerGitGithubTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
                 }
             }
         }
-    }
-}
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TarjetaPresentacion() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(all = 16.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(all = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TallerGitGithubTheme {
-        Greeting("Android")
+            Image(
+                painter = painterResource(R.drawable.fotoperfil),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(CircleShape)
+            )
+
+            Text(
+                text = "",
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            )
+
+            Text(
+                text = ""
+            )
+
+            Text(
+                text = ""
+            )
+
+            Text(
+                text = "",
+                modifier = Modifier.padding(top = 16.dp)
+            )
+
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                onClick = { },
+
+            ) {
+                Text("")
+            }
+        }
     }
 }
